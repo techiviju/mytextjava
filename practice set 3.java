4eed90a7c4a7199e9b5863b6644a589ab21b9daf
vijay chaudhari <113278961+techiviju@users.noreply.github.com>
2023-08-18 20:37:04 +0900

    Add files via upload

---
+++ practice set 3.java
@@ -0,0 +1,32 @@
+// this is a string practice set 3
+
+public class Main
+{
+	public static void main(String[] args) {
+        // problem no1
+        // String name="VIJAY";
+        // name=name.toLowerCase();
+        // System.out.println(name);
+    
+            // problem no 2
+        // String name="vijay anil chaudhari";
+        // name=name.replace(" ","_");
+        // System.out.println(name);
+        
+            //Problem no 3
+        // String name= "Dear <|name|> , Thanks a lot";
+        // name = name.replace("<|name|>" , "Vijay");
+        // System.out.println(name);
+        
+            // Problem no 4
+        // String name= "Vijay  Anil   Chaudhari";
+        // System.out.println(name.indexOf("  "));
+        // System.out.println(name.indexOf("   "));
+        
+            // Problem no 5
+        //  String name= "Dear Vijay \nyou are learned\njava course\tverry good !";
+        //  System.out.println(name);
+    
+    
+    	}
+}
---
+++ while_natural_nu.java
@@ -0,0 +1,40 @@
+/////**** using while loop to program 100 to 200 natural nu  *****/////
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int a=50;
+	    while(a<=100)
+	    {
+	        System.out.println(2*a);
+	        a++;
+	    }
+
+	////		USING DO-WHILE LOOP PRINT FIRST N NATURAL NU //////////
+	
+//		int a=1;
+// 		do{
+// 		    System.out.println(2*a);
+// 		    a++;
+// 		}while(a<=5);
+	    	
+
+		/// using for loog to print first n odd nu 
+		
+	   // for(int i=1;i<=10;i++)
+	   //  {
+	   //    System.out.println(2*i+1);
+	       
+	   //  }
+	
+		// print first n natural nu //
+	   //  for(int i=1;i<=5;i++)
+	   //  {
+	   //     System.out.println(2*i);
+	   //  }
+	    
+
+
+	}
+}
