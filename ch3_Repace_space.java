afcc925a47176196825406bcf7053dd6d9e70d5f
vijay chaudhari <113278961+techiviju@users.noreply.github.com>
2024-05-13 11:36:22 +0530

    Create ch3_Repace_space.java
    program to repace space and wrire _

---
+++ ch3_Repace_space.java
@@ -0,0 +1,6 @@
+public class Replace_space_underscore {
+    public static void main(String[] args) {
+        String name="vijay Anil chaudhari";
+        System.out.println(name.replace(" ", "_"));
+    }
+}
